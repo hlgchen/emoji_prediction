a73a8856447fa1b5693916765be4653bed3d7db3
hlgchen <hailong1998@googlemail.com>
2022-02-16 10:24:20 -0800

    Add model evaluation

--- emoji_embedding/cnn.py
+++ emoji_embedding/cnn.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 class ResnetExt(nn.Module):
     def __init__(self, output_dim):
@@ -19,4 +21,23 @@ class ResnetExt(nn.Module):
         x = F.relu(x)
         x = F.dropout(x, p=0.5)
         x = self.prediction_head(x)
+        return x
+
+
+class Img2Vec(nn.Module):
+    def __init__(self, pretrained_path):
+        """Uses resnet18 as backbone. Changes the last linear layer and outputs 300 dim vector for each image."""
+        super(Img2Vec, self).__init__()
+        self.resnet = torch.hub.load(
+            "pytorch/vision:v0.10.0", "resnet18", pretrained=False
+        )
+        self.resnet.fc = nn.Linear(512, 300)
+        print(
+            self.load_state_dict(
+                torch.load(pretrained_path, map_location=device), strict=False
+            )
+        )
+
+    def forward(self, x):
+        x = self.resnet(x)
         return x
