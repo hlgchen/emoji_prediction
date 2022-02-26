f5b07e097a1c72601912a2e65bb1675d64723c53
hlgchen <hailong1998@googlemail.com>
2022-02-26 00:19:15 -0800

    Add command to run

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 if [ "$1" = "vision" ]; then
 	python emoji_embedding/train_cnn.py
+elif [ "$1" = "embedding" ]; then
+	python emoji_embedding/create_embeddings.py
 else
 	echo "Invalid Option Selected"
 fi
