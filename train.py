4dc5c419c0f3915c18b9f592bd4a140ce8030f21
hlgchen <hailong1998@googlemail.com>
2022-02-27 12:16:33 -0800

    Add chunking to train dataloaders, models saved more frequently

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 from pprint import pprint
 from emoji_embedding.utils import model_summary
 from embert import EmbertLoss, SimpleEmbert, Accuracy
-from twemoji.twemoji_dataset import TwemojiData
+from twemoji.twemoji_dataset import TwemojiData, TwemojiDataChunks
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -24,98 +24,87 @@ def get_project_root():
 
 def train_model(
     model,
-    dataloaders,
+    dataloader_ls,
     criterion,
     optimizer,
     num_epochs,
     name,
-    save_every=2,
 ):
     """
-    Trains model with specified setup. Models are saved every 2 epoch.
-    Returns the model with best evaluation performance.
+    Trains model with specified setup. Models are saved after every chunk.
 
     Params:
-        - model {torch.nn.Module}: model to be trained. For the task at hand this is expected to be
-                                    a vision classification model that predicts logits for each class
-                                    of the image.
-        - dataloaders {dict}: dictionary containing the dataloaders. By default it is expected
-                                that this dictionary has "train" and "valid" as keys,
-                                with data loaders (containing the images) for the respective phases
-        - criterion {torch loss function}: for the task at hand, this should be cross entropy loss
+        - model {torch.nn.Module}: model to be trained. For the task at hand this isa version of embert
+        - dataloader_ls {list}: list of dictionaries containing the dataloaders. The dictionaries are expected
+                                to contain dataloaders for train and valid.
+        - criterion {torch loss function}: for the task at hand, this is the embertloss
         - optimizer {torch optimizer}: optimizer for training, i.e. adam
         - num_epochs {int}: number of epochs to train.
-
-    Returns:
-        - model {torch.nn.Module}: best model according to validation performance
-        - val_acc_history {list}: validation accuracies
+        - name {str}: name of the model to be saved
     """
-    start_time_training = time()
-
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_loss = 0.0
     acc = Accuracy()
 
     for epoch in range(num_epochs):
-        start_time_epoch = time()
-
-        print("Epoch {}/{}".format(epoch, num_epochs - 1))
+        print(f"Epoch {epoch}")
         print("-" * 10)
 
-        for phase in ["train", "valid"]:
-            if phase == "train":
-                model.train()  # Set model to training mode
-            else:
-                model.eval()  # Set model to evaluate mode
+        for chunk_i, dataloaders in enumerate(dataloader_ls):
+            start_time_chunk = time()
+            for phase in ["train", "valid"]:
+                if phase == "train":
+                    model.train()  # Set model to training mode
+                else:
+                    model.eval()  # Set model to evaluate mode
 
-            running_loss = 0.0
-            running_accuracy = 0
+                running_loss = 0.0
+                running_accuracy = 0
 
-            with tqdm(enumerate(dataloaders[phase])) as tbatch:
-                for i, batch in tbatch:
-                    start_time_batch = time()
+                with tqdm(enumerate(dataloaders[phase])) as tbatch:
+                    for i, batch in tbatch:
+                        start_time_batch = time()
 
-                    sentences_ls = batch[0]
-                    labels_ls = batch[1]
+                        sentences_ls = batch[0]
+                        labels_ls = batch[1]
 
-                    with torch.set_grad_enabled(phase == "train"):
-                        optimizer.zero_grad()
-                        outputs = model(sentences_ls, TRAIN_IDX)
-                        loss = criterion(outputs, labels_ls)
-                        if phase == "train":
-                            loss.backward()
-                            optimizer.step()
+                        with torch.set_grad_enabled(phase == "train"):
+                            optimizer.zero_grad()
+                            outputs = model(sentences_ls, TRAIN_IDX)
+                            loss = criterion(outputs, labels_ls)
+                            if phase == "train":
+                                loss.backward()
+                                optimizer.step()
 
-                    # statistics
-                    running_accuracy += acc(outputs, labels_ls)
-                    running_loss += loss.item() * len(labels_ls)
-                    tbatch.set_postfix(
-                        loss=loss.item() * len(labels_ls),
-                        running_loss=running_loss / (i + 1),
-                        running_accuracy=running_accuracy / (i + 1),
+                        # statistics
+                        running_accuracy += acc(outputs, labels_ls)
+                        running_loss += loss.item() * len(labels_ls)
+                        tbatch.set_postfix(
+                            loss=loss.item() * len(labels_ls),
+                            running_loss=running_loss / (i + 1),
+                            running_accuracy=running_accuracy / (i + 1),
+                        )
+
+                chunk_loss = running_loss / (i + 1)
+                chunk_accuracy = running_accuracy / (i + 1)
+
+                print(
+                    "{} Loss: {:.4f}, Accuracy: {:.4f}, took {}".format(
+                        phase,
+                        chunk_loss,
+                        chunk_accuracy,
+                        time() - start_time_batch,
                     )
+                )
 
-            epoch_loss = running_loss / (i + 1)
-            epoch_accuracy = running_accuracy / (i + 1)
-
+            time_elapsed = time() - start_time_chunk
             print(
-                "{} Loss: {:.4f}, Accuracy: {:.4f}, took {}".format(
-                    phase, epoch_loss, epoch_accuracy, time() - start_time_batch
+                "Chunk {}/{} FINISHED, took {:.0f}m {:.0f}s".format(
+                    chunk_i,
+                    len(dataloader_ls),
+                    time_elapsed // 60,
+                    time_elapsed % 60,
                 )
             )
-
-            if phase == "valid" and epoch_loss < best_loss:
-                best_loss = epoch_loss
-                best_model_wts = copy.deepcopy(model.state_dict())
-
-        time_elapsed = time() - start_time_epoch
-        print(
-            "Epoch {}/{} FINISHED, took {:.0f}m {:.0f}s".format(
-                epoch, num_epochs - 1, time_elapsed // 60, time_elapsed % 60
-            )
-        )
-        print("-" * 10)
-        if epoch % save_every == 0:
+            print("-" * 10)
             base = os.path.join(get_project_root(), f"trained_models/run1/")
             if not os.path.exists(base):
                 os.makedirs(base)
@@ -123,22 +112,10 @@ def train_model(
                 model.state_dict(),
                 os.path.join(
                     base,
-                    f"{name}_epoch{epoch}.ckpt",
+                    f"{name}_chunk{chunk_i+1}.ckpt",
                 ),
             )
             print("model saved")
-
-    time_elapsed = time() - start_time_training
-    print(
-        "Training complete in {:.0f}m {:.0f}s".format(
-            time_elapsed // 60, time_elapsed % 60
-        )
-    )
-    print("Best valid Acc: {:4f}".format(best_loss))
-
-    # load best model weights
-    model.load_state_dict(best_model_wts)
-    return model
 
 
 if __name__ == "__main__":
@@ -151,19 +128,22 @@ if __name__ == "__main__":
         model.load_state_dict(torch.load(pretrained_path, map_location=device))
     # pprint(model_summary(model))
 
-    train_data = TwemojiData("train", shuffle=True)
-    valid_data = TwemojiData("valid", shuffle=True)
-    dataloaders = {"train": train_data, "valid": valid_data}
+    train_data_chunks = TwemojiDataChunks(
+        "train", chunksize=128, shuffle=True, batch_size=64
+    )
+    valid_data = TwemojiData("valid", shuffle=True, batch_size=64, limit=128)
+    dataloader_ls = [
+        {"train": train_data, "valid": valid_data} for train_data in train_data_chunks
+    ]
 
     criterion = EmbertLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=5e-5)
 
     train_model(
         model,
-        dataloaders,
+        dataloader_ls,
         criterion,
         optimizer,
         num_epochs=1000,
         name="simpel_embert",
-        save_every=2,
     )
--- twemoji/twemoji_dataset.py
+++ twemoji/twemoji_dataset.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 
 
@@ -8,13 +9,19 @@ def get_project_root():
 
 
 class TwemojiData:
-    def __init__(self, dataset_type, nrows=None, shuffle=False, batch_size=32):
-        twemoji_path = os.path.join(
-            get_project_root(), f"twemoji/data/twemoji_{dataset_type}.csv"
-        )
-        self.df = pd.read_csv(
-            twemoji_path, usecols=["text_no_emojis", "emoji_ids"], nrows=nrows
-        )
+    def __init__(
+        self, data, nrows=None, shuffle=False, batch_size=64, limit=None, seed=1
+    ):
+        np.random.seed(seed)
+        if isinstance(data, str):
+            twemoji_path = os.path.join(
+                get_project_root(), f"twemoji/data/twemoji_{data}.csv"
+            )
+            self.df = pd.read_csv(
+                twemoji_path, usecols=["text_no_emojis", "emoji_ids"], nrows=nrows
+            )
+        else:
+            self.df = data
         self.df.emoji_ids = (
             self.df.emoji_ids.str[1:-1]
             .str.split(",")
@@ -24,6 +31,7 @@ class TwemojiData:
         self.shuffle = shuffle
         self.batch_size = batch_size
         self.n_samples = len(self.df)
+        self.limit = limit
 
     def get_lists(self):
         labels = self.df.emoji_ids.tolist()
@@ -35,9 +43,46 @@ class TwemojiData:
             self.df = self.df.sample(frac=1).reset_index(drop=True)
         text, labels = self.get_lists()
 
-        for start in range(0, self.n_samples, self.batch_size):
-            end = min(start + self.batch_size, self.n_samples)
+        limit = (
+            min(self.n_samples, self.limit)
+            if self.limit is not None
+            else self.n_samples
+        )
+        for start in range(0, limit, self.batch_size):
+            end = min(start + self.batch_size, limit)
             yield text[start:end], labels[start:end]
 
     def __getitem__(self, idx):
         return self.df.text_no_emojis.tolist()[idx], self.df.emoji_ids.tolist()[idx]
+
+
+class TwemojiDataChunks:
+    def __init__(
+        self, dataset_type, chunksize=64000, shuffle=False, batch_size=64, seed=1
+    ):
+        np.random.seed(seed)
+        twemoji_path = os.path.join(
+            get_project_root(), f"twemoji/data/twemoji_{dataset_type}.csv"
+        )
+        df = pd.read_csv(
+            twemoji_path,
+            usecols=["text_no_emojis", "emoji_ids"],
+        )
+        if shuffle:
+            df = df.sample(frac=1).reset_index(drop=True)
+
+        df_ls = [df[i : i + chunksize] for i in range(0, len(df), chunksize)]
+        self.data_ls = [
+            TwemojiData(df, shuffle=shuffle, batch_size=batch_size) for df in df_ls
+        ]
+        self.n_chunks = len(self.data_ls)
+
+    def __iter__(self):
+        for twemoji_dataset in self.data_ls:
+            yield twemoji_dataset
+
+    def __getitem__(self, idx):
+        return self.data_ls[idx]
+
+    def __len__(self):
+        return self.n_chunks
