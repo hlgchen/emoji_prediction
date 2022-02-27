285e4e1a1b51a623dcd314b1625a1bc21bb14928
hlgchen <hailong1998@googlemail.com>
2022-02-27 00:10:22 -0800

    Update run and gpu requirements

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ if [ "$1" = "vision" ]; then
 	python emoji_embedding/train_cnn.py
 elif [ "$1" = "embedding" ]; then
 	python emoji_embedding/create_embeddings.py
+elif [ "$1" = "main" ]; then
+	python train.py
 else
 	echo "Invalid Option Selected"
 fi
