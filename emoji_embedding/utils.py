b3741f8bb6ab88f9adb76f365ad4875e8f7b1285
hlgchen <hailong1998@googlemail.com>
2022-03-01 20:07:25 -0800

    Improve model summary function

--- emoji_embedding/utils.py
+++ emoji_embedding/utils.py
@@ -2,14 +2,21 @@ import numpy as np
 from pathlib import Path
 
 
-def model_summary(model):
+def model_summary(model, only_trainable=False, verbose=True):
     """Prints shapes of weights in each layer of the model.
     Prints total number of weights in the end."""
     total = 0
     for name, params in model.named_parameters():
-        print(name, params.size())
-        total += np.prod([x for x in params.size()])
-    print("total num_params", total)
+        if verbose:
+            print(f"{name}: {params.size()}, requires grad: {params.requires_grad}")
+        if only_trainable:
+            if params.requires_grad:
+                total += np.prod([x for x in params.size()])
+        else:
+            total += np.prod([x for x in params.size()])
+
+    text = "total num trainable params" if only_trainable else "total num_params"
+    print(text, total)
 
 
 def get_project_root():
