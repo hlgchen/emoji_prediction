9f254a2f18b091556152e59d1e58059a35202562
hlgchen <hailong1998@googlemail.com>
2022-03-02 21:11:15 -0800

    Change embbedding creation for emojis

--- emoji_embedding/create_embeddings.py
+++ emoji_embedding/create_embeddings.py
@@ -7,13 +7,11 @@ import pandas as pd
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
-from transformers import DistilBertTokenizer, DistilBertModel
+from sentence_transformers import SentenceTransformer
 import gensim.downloader as api
 
 from ee_model import Img2Vec
 from emoji_image_dataset import EmojiImageDataset
-
-from time import time
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -106,25 +104,13 @@ def create_vision_embedding():
     keys.csv. This means that the first embedding in embedding_files
     correspond to the first emoji in the csv files.
     """
+
+    print("creating vision embeddings")
+
     total_image_ds = EmojiImageDataset()
     model = load_emimem()
 
-    all_embeddings = create_embedding(
-        model, total_image_ds, "image_embeddings/all_embeddings"
-    )
-
-    # subset for training
-    train_df = (
-        total_image_ds.df.loc[~total_image_ds.df.zero_shot][
-            ["group_number", "emoji_id"]
-        ]
-        .drop_duplicates()
-        .sort_values(by="group_number", ascending=True)
-        .reset_index(drop=True)
-    )
-    train_idx = train_df.group_number.tolist()
-    train_embeddings = all_embeddings[train_idx]
-    save_data(train_embeddings, "image_embeddings/train_embeddings")
+    create_embedding(model, total_image_ds, "image_embeddings/all_embeddings")
 
 
 # ************************ create description embedding via word vectors***********************
@@ -210,6 +196,7 @@ def create_word_vector_embeddings():
     """Creates average word embeddings for all emojis (based of their description)
     The order in the embeddings is the same as in keys.csv
     """
+    print("creating word vector averages")
 
     df = load_and_process_description()
     model = "glove-twitter-200"
@@ -232,87 +219,35 @@ def create_word_vector_embeddings():
     all_embeddings = torch.stack([torch.from_numpy(x) for x in all_embeddings])
     train_embeddings = torch.stack([torch.from_numpy(x) for x in train_embeddings])
     save_data(all_embeddings, "word_vector_embeddings/all_embeddings")
-    save_data(train_embeddings, "word_vector_embeddings/train_embeddings")
 
 
-# ************************ create description embedding via BERT***********************
+# ************************ create description embedding via Sentence BERT***********************
 
 
-def get_model_embedding_wrapper(tokenizer, model):
-    """return get_model_embedding with predefined values for variables"""
+def create_distil_bert_embeddings():
 
-    def get_model_embedding(text_ls, mode="avg"):
-        """returns embedding of texts specified in text_ls dependent on mode
-        If mode is 'avg' last hidden states for all non padding tokens are averaged.
-        If mode is 'last' only the hidden state for the last non padding token is taken.
-        """
-        encoded_input = tokenizer(
-            text_ls, return_tensors="pt", truncation=True, padding=True
-        )
-        with torch.no_grad():
-            input_ids = encoded_input["input_ids"].to(device)
-            attention_mask = encoded_input["attention_mask"].to(device)
-            output = model(
-                input_ids=input_ids, attention_mask=attention_mask
-            ).last_hidden_state
+    print("creating sentence bert embeddings")
 
-        result_ls = []
-        if mode == "avg":
-            for i, l in enumerate(encoded_input.attention_mask.sum(dim=1).tolist()):
-                result_ls.append(output[i, :l].mean(dim=0))
-        elif mode == "last":
-            for i, l in enumerate(encoded_input.attention_mask.sum(dim=1).tolist()):
-                result_ls.append(output[i, l - 1])
-        else:
-            print("mode is not valid")
-        return result_ls
-
-    return get_model_embedding
-
-
-def get_bert_embeddings(df, get_model_embedding, batch_size=128):
-
-    filler = "\u25A1" * 3
+    df = load_and_process_description()
+    filler = "\u25A1"
     s = df.emjpd_emoji_name_og.fillna("") + filler
     s += df.emjpd_aliases.fillna("") + filler
     s += df.emjpd_description_main.fillna("").str.replace("\n", filler) + filler
     s_ls = s.tolist()
 
-    embedding_ls = []
+    model_name = "all-mpnet-base-v1"
+    model = SentenceTransformer(model_name)
+    model.max_seq_length = 512
 
-    for i in range(batch_size, len(s_ls), batch_size):
-        start = time()
-        embedding_ls += get_model_embedding(s_ls[i - batch_size : i])
-        print(f"processed {i- batch_size} to {i}, took {time() - start}")
-    start = time()
-    embedding_ls += get_model_embedding(s_ls[i:])
-    print(f"processed {i} to {len(s_ls)}, took {time() - start}")
-
-    # embedding_dict = {
-    #     k: v for k, v in zip(df.emoji_char.tolist()[: len(embedding_ls)], embedding_ls)
-    # }
-    embeddings = torch.stack(embedding_ls)
-    return embeddings
-
-
-def create_distil_bert_embeddings():
-
-    df = load_and_process_description()
-
-    base_model_name = (
-        "distilbert-base-uncased"  # the tokenizer does not know the emojis
+    embeddings = model.encode(
+        s_ls,
+        show_progress_bar=True,
+        convert_to_tensor=True,
+        device=device,
+        normalize_embeddings=True,
     )
-    tokenizer = DistilBertTokenizer.from_pretrained(base_model_name)
-    model = DistilBertModel.from_pretrained(base_model_name)
-    model.eval()
-    model.to(device)
 
-    get_model_embedding = get_model_embedding_wrapper(tokenizer, model)
-    all_embeddings = get_bert_embeddings(df, get_model_embedding, 128)
-    train_embeddings = all_embeddings[(~df.zero_shot).tolist()]
-
-    save_data(all_embeddings, "bert_embeddings/all_embeddings")
-    save_data(train_embeddings, "bert_embeddings/train_embeddings")
+    save_data(embeddings, "bert_embeddings/all_embeddings")
 
 
 if __name__ == "__main__":
