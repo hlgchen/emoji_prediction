94fd41a5916bcc0b27a4716c78e6cff8410d990a
hlgchen <hailong1998@googlemail.com>
2022-03-06 12:21:03 -0800

    Add balanced option for evaluation

--- evaluate.py
+++ evaluate.py
@@ -118,6 +118,7 @@ if __name__ == "__main__":
             "extra_zero",
             "extra_zero_v2",
             "extra_zero_v2_min_2",
+            "balanced_test_v2",
         ],
     )
     argp.add_argument(
